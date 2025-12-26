e11e8da709ecf0074fe6248f1db41a214fef33e4
anandg112 <anandg112@gmail.com>
2025-12-25 23:04:50 -0500

    chore: add api documenter and api-extractor

--- packages/models/src/seed-packet-collection.model.ts
+++ packages/models/src/seed-packet-collection.model.ts
@@ -1,5 +1,9 @@
 import type { SeedPacketModel } from './seed-packet.model.js'
 
+/**
+ * A collection of seed packets
+ * @public
+ */
 export interface SeedPacketCollectionModel {
 	seedPackets: SeedPacketModel[]
 }
